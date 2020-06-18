10c269021fbe5f262e42939906d01c0d9e34e03b
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-06-18 12:38:20 +0200

    Initial DB connect

---
+++ DB.py
@@ -0,0 +1,16 @@
+
+import snowflake.connector as sf
+
+def db_cursor():
+
+    # Create Cursor:
+    ctx = sf.connect(
+        #host='https://solita.eu-central-1.snowflakecomputing.com/console',
+        account='xxx',
+        user='xxx',
+        password='xxx',
+        database='xxx',
+        schema='xxx'
+    )
+    cs = ctx.cursor()
+    return ctx, cs
