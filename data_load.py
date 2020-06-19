f55fa160a04eb6ae55bf09a9690f7feb45dfc3ad
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-06-19 15:25:07 +0200

    Add Function to data load

--- data_load.py
+++ data_load.py
@@ -3,3 +3,17 @@ import os
 import json
 import pickle
 import pandas as pd
+
+def create_table(name, cursor, path = None, type_='json'):
+    '''
+    python design pattern: include cursor in function explicity
+    '''
+    #if type_=='CSV':
+
+    if type_=='json':
+        #create_table = "create or replace table myjsontable (json_data variant)"
+        table_sql = 'create table {} IF NOT EXISTS (json_data variant)'.format(str(name))
+
+
+    table_result = cursor.execute(table_sql)
+    print(table_result.fetchall())
