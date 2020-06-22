6d8234b87d5029584b9b17f6999bbbe9b89bf51d
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-06-22 17:10:55 +0200

    add utils function

--- utils.py
+++ utils.py
@@ -5,3 +5,15 @@ def clear_comma(df, col):
 
     df[col] = df[col].apply(lambda x: re.sub(',', '.', str(x)))
     return df
+
+def pickle_to_json(path):
+
+    file = open(path, 'rb')
+
+    # dump information to that file
+    data = pickle.load(file)
+    data_json = json.dumps(data)
+
+    json_path = path.replace(".pickle", ".json")
+    with open(json_path, 'w') as outfile:
+        json.dump(data_json, outfile, sort_keys=True, indent=4)
