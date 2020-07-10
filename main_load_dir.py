90824d3d87fd91bc760281ad34b676640ef2236e
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-07-10 15:05:30 +0200

    add creation skeleton

--- main_load_dir.py
+++ main_load_dir.py
@@ -9,3 +9,12 @@ target = '.../bike_test.csv'
 format_name = 'xxx'
 table_name = 'xxx'
 stage_name = 'xxx'
+
+
+file_list = [file for file in os.listdir(path_) if '.json' in str(file)]
+file_list.sort()
+
+
+create_table(table_name)
+create_format(format_name)
+create_stage(stage_name, format_name)
