145e3e0d7db591642c1f348610c7ba8f7a995267
RichardWie1and <61945371+RichardWie1and@users.noreply.github.com>
2020-08-04 13:43:48 +0200

    add arg parser to main

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 from data_processing import *
+import argparse
 
 
-source = '.../OPENDATA_BOOKING_CALL_A_BIKE.csv'
-target = '.../bike_test.csv'
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--path_", type=str,  help="Dictionary for data")
+    parser.add_argument("--format_name", type=str,  help="name of format")
+    parser.add_argument("--table_name", type=str,  help="name of table")
+    parser.add_argument("--stage_name", type=str,  help="name of stage")
 
 
+    file_list = [file for file in os.listdir(args.path_) if '.json' in str(file)]
+    file_list.sort()
 
-format_name = 'xxx'
-table_name = 'xxx'
-stage_name = 'xxx'
+    print(file_list)
 
-
-file_list = [file for file in os.listdir(path_) if '.json' in str(file)]
-file_list.sort()
-
-print(file_list)
-
-cs = db_cursor()
+    cs = db_cursor()
 
 
-create_table(table_name)
-create_format(format_name)
-create_stage(stage_name, format_name)
+    create_table(args.table_name)
+    create_format(args.format_name)
+    create_stage(args.stage_name, args.format_name)
 
-for _, file_name in enumerate(file_list):
+    for _, file_name in enumerate(file_list):
 
-    # pickle to json:
-    file_path = path_ + file_name
-    # load from local to table
-    stage_file(stage_name, file_path)
-    list_stage(stage_name)
-    check_table(table_name)
+        # pickle to json:
+        file_path = args.path_ + file_name
+        # load from local to table
+        stage_file(args.stage_name, args.file_path)
+        list_stage(args.stage_name)
+        check_table(args.table_name)
 
-copy_files(table_name, stage_name, format_name, file_list)
-remove_stage(stage_name)
+    copy_files(args.table_name, args.stage_name, args.format_name, file_list)
+    remove_stage(args.stage_name)
 
-db_close(cs)
+    db_close(cs)
